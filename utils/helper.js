a651740463db49bc9c2d4463bb6d85a6449dba9f
ademonteverde <108604045+ademonteverde@users.noreply.github.com>
2023-10-02 22:16:27 -0400

    updated helpers

--- utils/helper.js
+++ utils/helper.js
@@ -1,6 +1,8 @@
-// // Create a helper function to format the date
-// Handlebars.registerHelper('formatDate', function(date) {
-//     const options = { year: 'numeric', month: 'short', day: 'numeric' };
-//     return new Date(date).toLocaleDateString(undefined, options);
-//   });
+const Handlebars = require('handlebars');
+
+// Create a helper function to format the date
+Handlebars.registerHelper('formatDate', function(date) {
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    return new Date(date).toLocaleDateString(undefined, options);
+  });
   
